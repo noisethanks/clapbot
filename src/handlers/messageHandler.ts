cc3a1058afbaecfb5340454ffd9d47b79e8bf024
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2025-07-21 21:59:24 -0700

    refactor: move channel IDs to env vars

--- src/handlers/messageHandler.ts
+++ src/handlers/messageHandler.ts
@@ -2,11 +2,9 @@ import { Message } from "discord.js";
 import { moderate } from "../moderation/moderate";
 import { logViolation } from "../utils/logger";
 
-const MODERATED_GATE_CHANNEL_ID = "1396690898615537747";
-
 export async function handleMessage(message: Message): Promise<void> {
   if (message.author.bot || !message.guild) return;
-  if (message.channel.id === MODERATED_GATE_CHANNEL_ID) {
+  if (message.channel.id === process.env.MODERATED_GATE_CHANNEL_ID) {
     if (!message.interactionMetadata) {
       const reply = await message.author.send({
           content: `🚫 Please use the /post command to submit content.`,
