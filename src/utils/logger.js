7e5155b474d4352f3274f55ed63b79d2db7cc62c
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2025-07-21 22:11:34 -0700

    Deleted js build files.

--- src/utils/logger.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logViolation = logViolation;
-function logViolation(message, reason) {
-    console.log(`🚫 [${reason}] ${message.author.tag}: ${message.content}`);
-}
